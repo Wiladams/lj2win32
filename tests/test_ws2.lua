0e28de99da9c7ab9eaed1e13e851deb6becde327
William Adams <git@nanotechstyles.com>
2016-10-30 19:38:25 -0700

    fixing up ws2_32.lua

---
+++ tests/test_ws2.lua
@@ -0,0 +1,14 @@
+package.path = "../?.lua;"..package.path;
+local bit = require("bit")
+local bnot = bit.bnot
+
+local ffi = require("ffi")
+local ws2 = require("win32.ws2_32")
+
+print(INVALID_SOCKET)
+sock = ffi.new("SOCKET", -1)
+print(sock)
+sock = ffi.new("SOCKET", bnot(0))
+print(sock)
+sock = ffi.new("uint64_t", -1)
+print(sock)
