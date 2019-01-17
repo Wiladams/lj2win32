051cbb659e97c80b78b12e77c7cb1612c17f0a86
William Adams <wiladams@microsoft.com>
2019-01-17 08:33:59 -0800

    work on delimeted string parsing start on setupapi

--- tests/test_strdelim.lua
+++ tests/test_strdelim.lua
@@ -1,6 +1,18 @@
 local strdelim = require("strdelim")
 
+local function testMultiNull()
 local astring="value1\0value2\0value3\0value4\0\0"
 for i, str in strdelim.multinullstrings(astring) do
     print(i,str)
 end
+end
+
+local function testRegPath()
+    local astring = "HKEY_LOCAL_MACHINE\\HARDWARE\\DESCRIPTION\\System"
+    for i,str in strdelim.delimvalues(astring, #astring, string.byte('\\')) do
+        print(i,str)
+    end
+end
+
+--testMultiNull();
+testRegPath()
