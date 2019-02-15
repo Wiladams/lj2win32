8c5e66597a5c200bd10ed66dd06b7e3c888d303a
William Adams <wiladams@microsoft.com>
2019-02-15 06:53:36 -0800

    playing with nt_dll system information

--- tests/test_dll_system.lua
+++ tests/test_dll_system.lua
@@ -5,6 +5,12 @@ local ffi = require("ffi")
 require("win32.sdkddkver")
 local system = require("ntdll_system")
 
-print(system.getSystemInformation("SystemBasicInformation"))
+for key, value in pairs(system.SYSTEM_INFORMATION_CLASS) do
+    --print(key, system.getSystemInformation(value))
+    local value, err = system.getSystemInformation(value)
+    if value then
+        print(key, value)
+    end
+end
 
-print("sizeof(SYSTEM_PROCESS_INFORMATION): ", string.format("0x%x", ffi.sizeof("SYSTEM_PROCESS_INFORMATION")))
+--print("sizeof(SYSTEM_PROCESS_INFORMATION): ", string.format("0x%x", ffi.sizeof("SYSTEM_PROCESS_INFORMATION")))
