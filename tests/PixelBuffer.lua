e15cc21ef46f081013c55af71d8f0442b81dcdbf
William Adams <william_a_adams@msn.com>
2019-02-18 08:56:13 -0800

    add Pixel32 to PixelBuffer

--- tests/PixelBuffer.lua
+++ tests/PixelBuffer.lua
@@ -1,22 +1,112 @@
 local ffi = require("ffi")
+local bit = require("bit")
+local lshift, rshift = bit.lshift, bit.rshift
+local band, bor = bit.band, bit.bor
 
---[[
-local function Array2D(columns, rows)
-    local arrtype = ffi.typeof("int[$][$]", rows, columns)
-    return arrtype()    -- initialized with all 0
-end
---]]
+-- Store the matrix kinds so we don't create a new
+-- type declaration for every pixel buffer
+local matrix_kinds = {}
 
-local Array2D = ffi.typeof("uint32_t[$][$]")
+ffi.cdef[[
+typedef union Pixel32_t {
+    struct {
+        uint8_t Red;
+        uint8_t Green;
+        uint8_t Blue;
+        uint8_t Alpha;
+    };
+    uint32_t cref;
+} Pixel32;
+]]
+
+
 
 local PixelBuffer = {}
+setmetatable(PixelBuffer, {
+    __call = function (self, ...)
+        return self:new(...)
+    end
+})
 local PixelBuffer_mt = {
-
+    __index = PixelBuffer
 }
 
-function PixelBuffer.init(self, pixels)
-    local arrtype = ffi.typeof("uint32_t[$][$]", rows, columns)
-    local obj = {
-        pixels = 
-    }
-local p1 = Array2D(480, 640)
+function PixelBuffer.init(self, params)
+
+    local obj = params or {}
+    setmetatable(obj, PixelBuffer_mt)
+
+    return obj;
+end
+
+function PixelBuffer.new(self, width, height)
+    -- lookup the type of we've already created it
+    local typemoniker = string.format("Pixel32:[%d,%d]", height, width);
+    local matrixType = matrix_kinds[typemoniker]
+    if not matrixType then
+        matrixType = ffi.typeof("Pixel32[$][$]", height, width)
+        matrix_kinds[typemoniker] = matrixType
+    end
+
+    local pixels = matrixType()
+
+    return self:init({
+        Pixels = pixels, 
+        Width=width, 
+        Height=height, 
+        Kind="uint32_t"})
+end
+
+function PixelBuffer.RGB(self, r,g,b)
+    return ffi.new("Pixel32", {r,g,b,0})
+end
+
+function PixelBuffer.RGBA(self, r,g,b,a)
+    return  ffi.new("Pixel32",{r,g,b,a})
+end
+
+
+function PixelBuffer.set(self, x, y, pix)
+    self.Pixels[y][x] = pix
+    return self
+end
+
+function PixelBuffer.get(self, x, y)
+    return self.Pixels[y][x]
+end
+
+local black = PixelBuffer:RGB(0,0,0,0)
+
+--[[
+    set the entire pixel buffer to the specified
+    pixel value.
+
+    This is a quick way to clear a buffer without 
+    doing any sort of blending operations.
+]]
+function PixelBuffer.clear(self, pix)
+    pix = pix or black
+    local pixels = self.Pixels
+
+    for y=0,self.Height-1 do
+        for x=0,self.Width-1 do
+            pixels[y][x] = pix
+        end
+    end
+end
+
+--[[
+    copy one pixel buffer to another
+    without any blending operation
+
+    need to do clipping
+]]
+function PixelBuffer.srcOver(self, src, dstX, dstY)
+    for y=0,src.Height-1 do
+        for x=0,src.Width-1 do
+            self.Pixels[dstY+y][dstX+x] = src.Pixels[y][x]
+        end
+    end
+end
+
+return PixelBuffer
---
+++ tests/test_pixelbuffer.lua
@@ -0,0 +1,122 @@
+local ffi = require("ffi")
+
+local PixelBuffer = require("PixelBuffer")
+
+
+function lerp(low, high, x)
+    return low + x*(high-low)
+end
+
+function map(x, olow, ohigh, rlow, rhigh)
+    rlow = rlow or olow
+    rhigh = rhigh or ohigh
+    return rlow + (x-olow)*((rhigh-rlow)/(ohigh-olow))
+end
+
+--[[
+    opacity table allows you to use lookups to determine
+    an opacity value without doing multiplication.
+
+    createOpacityTable() essentially pre-calculates the destination
+    value given an opacity and value.
+
+    The table is two dimensional (256x256)
+    Each 'row' represents an opacity level (0-255)
+    Each column' represents a source value (0 -255)
+    The element represents the pre-multiplied value for the given
+    opacity/value combination.
+
+    Usage:
+    local tab = createOpacityTable()
+
+    -- perform alpha blending
+    local alpha = 127
+    function blend(pb, src, alpha)
+        for y=0,src.Height-1 do
+            for x=0,src.Width-1 do
+                -- or something like it
+                pb:set(x,y, tab[alpha][src:get(x,y).red])
+            end
+        end
+    end
+]]
+local function createOpacityTable()
+    local tab = ffi.new("uint8_t[256][256]")
+    for opacity =0,255 do    
+        for value=0,255 do
+            local opvalue = map(value, 0,255, 0, opacity)
+            tab[opacity][value] = opvalue
+        end
+    end
+    return tab
+end
+
+
+local function printPixelBuffer(pb)
+    for y =0, pb.Height-1 do
+        for x = 0, pb.Width-1 do
+            io.write(string.format('%2d ',pb.Pixels[y][x].cref))
+        end
+        print()
+    end
+end
+
+
+local function hline(pb, x, y, len, pix)
+    for i=0,len-1 do
+        pb.Pixels[y][x+i] = pix
+    end
+
+    return true
+end
+
+local function vline(pb, x, y, len, pix)
+    for i=0,len-1 do
+        pb.Pixels[y+i][x] = pix
+    end
+
+    return true
+end
+
+local function fillRect(pb, x, y, width, height, pix)
+    for i=0,height-1 do
+        hline(pb, x, y+i, width, pix)
+    end
+
+    return true
+end
+
+local function frameRect(pb, x, y, width, height, pix)
+    hline(pb, x,y,width,pix)
+    hline(pb, x, y+height-1, width,pix)
+    vline(pb, x, y, height,pix)
+    vline(pb, x+width-1, y, height, pix)
+end
+
+
+local function test_srcover()
+    local frameBuffer = PixelBuffer(20,10)
+    local image = PixelBuffer(4,3)
+
+    -- make framebuffer background white
+    local bgPix = PixelBuffer:RGB(88,0,0)
+    frameBuffer:clear(bgPix)
+
+    -- give the image some pixels
+    local c = PixelBuffer:RGB(2, 0,0)
+    image:clear(c)
+
+    -- copy image to framebuffer
+    local black = PixelBuffer:RGB(0,0,0)
+    frameBuffer:srcOver(image, 1,1)
+    frameBuffer:srcOver(image, 3,3)
+    frameBuffer:srcOver(image, 10,5)
+    frameRect(frameBuffer, 9,4,5,4,black)
+
+    printPixelBuffer(image)
+    printPixelBuffer(frameBuffer)
+end
+
+
+test_srcover()
+--createOpacityTable()
