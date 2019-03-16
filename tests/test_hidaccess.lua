9d667c0556ea6d585d7f7ad2583d80f0578e70f8
William Adams <wiladams@microsoft.com>
2019-03-16 07:23:22 -0700

    add hidusagedb and perlin noise

--- tests/test_hidaccess.lua
+++ tests/test_hidaccess.lua
@@ -10,9 +10,16 @@ local HIDInterface = require("hidinterface")
 local HIDDevice = require("HIDDevice")
 local spairs = require("spairs")
 local usbvendorlist = require("usbvendorlist")
+local hidusagedb = require("hidusagedb")
 
 local hider = HIDInterface();
 
+local function lookupUsage(usagePage, usage)
+    local page = hidusagedb.usage[usagePage]
+    if not page then return string.format("0x%x", usage) end
+
+    return page[usage] or string.format("0x%x", usage)
+end
 
 local function printDict(dict, name)
     name = name or "Dictionary"
@@ -20,8 +27,15 @@ local function printDict(dict, name)
     for k,v in spairs(dict) do
         if k == "VendorID" then 
             k = "VENDOR" 
-            v = usbvendorlist[v]
-        end
+            v = usbvendorlist[v] or string.format("0x%x", v)
+        elseif k == "Usage" then
+            k = "USAGE"
+            v = lookupUsage(dict.UsagePage, v)
+        elseif k == "UsagePage" then
+            k = "USAGEPAGE"
+            v = hidusagedb.pages[v] or string.format("0x%x",v)
+        end 
+
         print(string.format("%15s: %s", k,tostring(v)))
     end
     print("---------------------")
--- tests/test_p5_noise2.lua
+++ tests/test_p5_noise2.lua
@@ -2,7 +2,10 @@ package.path = "../?.lua;"..package.path;
 
 require("p5")
 
-local noiseScale=0.03;
+local perlin = require("stb_perlin_noise")
+local noise = perlin.noise3
+
+local noiseScale=0.005;
 
 
 function draw()
@@ -13,7 +16,9 @@ function draw()
     background(0);
     for x=0, width-1 do
         local noiseVal = noise((mouseX+x)*noiseScale, mouseY*noiseScale);
-        stroke(noiseVal*255);
+        local g = noiseVal * 255    -- scale color
+        print(noiseVal, g)
+        stroke(g);
         line(x, mouseY+noiseVal*80, x, height);
     end
 end
